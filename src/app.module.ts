3575b2ac511e76f5ba9b8fd5d20a90ee270f7f5f
Adam Skrzypinski <adamskrzy@gmail.com>
2023-10-24 19:09:05 +0200

    Database configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './config/config';
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: config.dbHost,
+      port: 3306,
+      username: config.dbUser,
+      password: config.dbPassword,
+      database: 'warehouse-db',
+      entities: [],
+      bigNumberStrings: false,
+      logging: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
